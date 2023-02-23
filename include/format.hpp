5ebf84573f065126ab41dedb1d296605bbe373ac
Andrey Moura <andreymds88@gmail.com>
2023-02-23 20:47:57 -0300

    Add submodules and CMakeLists.txt

---
+++ include/format.hpp
@@ -0,0 +1,13 @@
+#ifdef __linux__
+    #define FMT_HEADER_ONLY
+    #include "fmt/format.h"
+
+    #define USE_FMT_FORMT
+
+    namespace std
+    {
+        using namespace fmt;
+    };
+#else
+    #include <format>
+#endif
