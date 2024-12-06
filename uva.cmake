feaa945cb08420b3d155abc3d860882efd873731
Andrey Moura <andreymds88@gmail.com>
2024-12-06 20:22:35 -0300

    wip: Clone the repository if it does not exists

--- uva.cmake
+++ uva.cmake
@@ -1,3 +1,19 @@
 macro (uva_library uva_library_name)
-    include(${UVA_ROOT_DIR}/include/${uva_library_name}/CMakeLists.txt)
+    set(LIBRARY_FOLDER ${UVA_ROOT_DIR}/include/${uva_library_name})
+    
+    if(NOT EXISTS ${LIBRARY_FOLDER})
+        execute_process(
+            COMMAND git submodule update include/${uva_library_name}
+            WORKING_DIRECTORY ${UVA_ROOT_DIR}
+            RESULT_VARIABLE RESULT
+            OUTPUT_VARIABLE OUTPUT
+            ERROR_VARIABLE ERROR
+        )
+
+        if(NOT ${RESULT} EQUAL 0)
+            message(FATAL_ERROR "Error cloning ${uva_library_name} library: ${ERROR}")
+        endif()
+    endif()
+
+    include(${LIBRARY_FOLDER}/CMakeLists.txt)
 endmacro ()
