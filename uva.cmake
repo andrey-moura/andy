9ed32e2a63673acbb2db1bcca779fbea1d9d4b7e
Andrey Moura <andreymds88@gmail.com>
2024-12-06 20:32:11 -0300

    wip: log to understand what is happening

--- uva.cmake
+++ uva.cmake
@@ -1,7 +1,9 @@
 macro (uva_library uva_library_name)
     set(LIBRARY_FOLDER ${UVA_ROOT_DIR}/include/${uva_library_name})
     
-    if(NOT EXISTS ${LIBRARY_FOLDER}/CMakeLists.txt)
+    if(EXISTS ${LIBRARY_FOLDER}/CMakeLists.txt)
+        message(STATUS "${LIBRARY_FOLDER}/CMakeLists.txt exists")
+    else()
         execute_process(
             COMMAND git submodule update include/${uva_library_name}
             WORKING_DIRECTORY ${UVA_ROOT_DIR}
@@ -9,6 +11,10 @@ macro (uva_library uva_library_name)
             OUTPUT_VARIABLE OUTPUT
             ERROR_VARIABLE ERROR
         )
+
+        message(STATUS "Cloning ${uva_library_name} library: ${OUTPUT}")
+        message(STATUS "Cloning ${uva_library_name} library: ${ERROR}")
+        message(STATUS "Cloning ${uva_library_name} library: ${RESULT}")
 
         if(NOT ${RESULT} EQUAL 0)
             message(FATAL_ERROR "Error cloning ${uva_library_name} library: ${ERROR}")
